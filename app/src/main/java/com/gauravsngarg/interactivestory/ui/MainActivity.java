f86a1bd6ade985245a9c846e02dbd504d2b95271
gauravsngarg <gauravsngarg@gmail.com>
2016-04-26 23:22:28 +0530

    Add Store Activity

--- app/src/main/java/com/gauravsngarg/interactivestory/ui/MainActivity.java
+++ app/src/main/java/com/gauravsngarg/interactivestory/ui/MainActivity.java
@@ -1,19 +1,23 @@
-package com.gauravsngarg.interactivestory;
+package com.gauravsngarg.interactivestory.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import com.gauravsngarg.interactivestory.R;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mNameField;
     private Button mStartButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         mNameField = (EditText) findViewById(R.id.nameEditText);
         mStartButton = (Button) findViewById(R.id.startbutton);
@@ -22,8 +26,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String name = mNameField.getText().toString();
-                Toast.makeText(MainActivity.this,name,Toast.LENGTH_LONG).show();
+                StartStory(name);
             }
         });
+
+
+    }
+
+    private void StartStory(String name) {
+        Intent intent = new Intent(this,StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name),name);
+        startActivity(intent);
     }
 }
